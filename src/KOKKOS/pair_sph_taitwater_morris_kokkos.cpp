7a496735dfc15157d2e69cc51fce274e7468ae67
moein <moeinnaderi.iut@gmail.com>
2025-08-20 17:42:21 -0500

    Add print stmt to taitmor kokkos

--- src/KOKKOS/pair_sph_taitwater_morris_kokkos.cpp
+++ src/KOKKOS/pair_sph_taitwater_morris_kokkos.cpp
@@ -73,6 +73,7 @@ PairSPHTaitwaterMorrisKokkos<DeviceType>::~PairSPHTaitwaterMorrisKokkos()
 template<class DeviceType>
 void PairSPHTaitwaterMorrisKokkos<DeviceType>::compute(int eflag_in, int vflag_in)
 {
+	fprintf(screen, "compute() called on PairSPHTaitwaterMorrisKokkos\n");
   eflag = eflag_in;
   vflag = vflag_in;
 
