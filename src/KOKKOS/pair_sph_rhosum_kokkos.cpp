7595a9c48566891da9d2570cb76611a6fc6c1415
moein <moeinnaderi.iut@gmail.com>
2025-08-27 15:15:51 -0500

    Add back the ev_tally() call in rhosum kk

--- src/KOKKOS/pair_sph_rhosum_kokkos.cpp
+++ src/KOKKOS/pair_sph_rhosum_kokkos.cpp
@@ -69,7 +69,7 @@ template<class DeviceType>
 void PairSPHRhoSumKokkos<DeviceType>::compute(int eflag_in, int vflag_in)
 {
 	fprintf(screen, "compute() called on PairSPHRhoSumKokkos\n");
-  //ev_init(eflag,vflag,0);  //safe, but is it required? -Moein
+  ev_init(eflag,vflag,0);  //safe, but is it required? -Moein
 
   atomKK->sync(execution_space,datamask_read);
 
